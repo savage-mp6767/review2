4fe0797f9420ac5cda5a24daeb30a7c9fdf03d6b
savage-mp6767 <liegrowlmuzak@gmail.com>
2014-10-23 15:56:18 -0500

    yay
    list comprehensions and string manipulations make this easy to do
    without a loop

--- review.py
+++ review.py
@@ -18,3 +18,6 @@
 # Remember how range can be used to create the list [2,4,6,8]
 # When complete, commit to github and submit a pull request!
 
+a = '\n'.join([str(i) for i in range(2,10,2)])
+a += '\nwho do we appreciate?'
+print a
